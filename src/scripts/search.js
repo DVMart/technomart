f57bb09795061fbaa4f351e4670fef68c2d7e17f
DVMart <dvmart8@gmail.com>
2025-12-16 18:01:05 +0300

    В поиске for заменен на forEach

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -4,13 +4,9 @@ export function searchProducts() {
     query = searchInput.value.toUpperCase();
     containers = document.querySelector("#product-list").querySelectorAll(".instrument");
 
-    for (i = 0; i < containers.length; i++) {
-        h3 = containers[i].querySelector("h3");
-        txtValue = h3.textContent || h3.innerText;
-        if (txtValue.toUpperCase().indexOf(query) > -1) {
-            containers[i].style.display = "";
-        } else {
-            containers[i].style.display = "none";
-        }
-    }
+    containers.forEach(container => {
+        const h3 = container.querySelector("h3");
+        const txtValue = (h3 && (h3.textContent || h3.innerText)) || "";
+        container.style.display = txtValue.toUpperCase().indexOf(query) > -1 ? "" : "none";
+    });
 }
