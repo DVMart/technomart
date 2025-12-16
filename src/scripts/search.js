1d874da03ce877dbbe6478be7504bc53762cfb19
DVMart <dvmart8@gmail.com>
2025-12-16 17:37:08 +0300

    Восстановлен поиск

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -2,10 +2,10 @@ export function searchProducts() {
     let query, containers, i, h3, txtValue, products;
     const searchInput = document.querySelector("#search-input");
     query = searchInput.value.toUpperCase();
-    containers = document.querySelector("#product-list").querySelectorAll("h3");
+    containers = document.querySelector("#product-list").querySelectorAll(".instrument");
 
     for (i = 0; i < containers.length; i++) {
-        h3 = containers[i].getElementsByTagName("h3")[0];
+        h3 = containers[i].querySelector("h3");
         txtValue = h3.textContent || h3.innerText;
         if (txtValue.toUpperCase().indexOf(query) > -1) {
             containers[i].style.display = "";
